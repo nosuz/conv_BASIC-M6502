cb94b8805f34acd1e3440326ab098155503046f3
Norio Suzuki <norisuzu@gmail.com>
2026-01-12 17:54:56 +0900

    Added shell script to assemble and link.

---
+++ convert.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+python m6502_convert.py -o ./m6502.s BASIC-M6502/m6502.asm
+
+# ca65 m6502.s -o m6502.o
+ca65 m6502.s --cpu 6502 -l m6502.lst -o m6502.o
+ld65 m6502.o -o m6502.bin -C linker.cfg
