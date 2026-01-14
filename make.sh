08a7ff7ad3eaed489919337e2ed97d24a76bea5d
Norio Suzuki <norisuzu@gmail.com>
2026-01-15 06:42:31 +0900

    Rename build script and converter to reflect MACRO-10 to ca65

--- make.sh
+++ make.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python m6502_convert.py -o ./m6502.s BASIC-M6502/m6502.asm
+python macro10_to_ca65.py -o ./m6502.s BASIC-M6502/m6502.asm
 
 # ca65 m6502.s -o m6502.o
 ca65 m6502.s --cpu 6502 -l m6502.lst -o m6502.o
